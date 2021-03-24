bfe1437898de3fb5772c3c942fa46ba2e2a05d79
Rederxz <1148002188@qq.com>
2021-03-24 14:48:46 +0800

    add prepare

--- data/__init__.py
+++ data/__init__.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
-def prepare(ds, batch_size, shuffle=False, augment=False, transform, autotune):
+def prepare(ds, batch_size, shuffle=False, augment=False, transform):
     if shuffle:
-        ds = ds.shuffle(1000)
+        ds = ds.shuffle(len(ds)) # a large enough buffer size is required 
     
     ds = ds.batch(batch_size)
 
     if augment:
         ds = ds.map(lambda x, y: (transform(x, training=True), y),
-                    num_parallel_calls=autotune)
+                    num_parallel_calls=tf.data.AUTOTUNE)
     
-    return ds.prefetch(buffer_size=autotune)
+    return ds.prefetch(buffer_size=tf.data.AUTOTUNE)
