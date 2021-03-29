148ee01d4236f419947f730d3b75b27b45beda35
Rederxz <1148002188@qq.com>
2021-03-29 10:26:49 +0800

    Add log plotter

--- utils/__init__.py
+++ utils/__init__.py
@@ -13,21 +13,22 @@ def set_seed(seed=0):
     np.random.seed(seed)
     tf.random.set_seed(seed)
 
-def v_model(model, file_name=None):
+def plot_model(model, file_name=None):
     plot_model(model, to_file=file_name, show_shapes=True, show_layer_names=False)
 
-
-# def plot_result(log, yticks=np.arange(0., 1., 0.1), acc_to_error=False): # log: a tuple contains train log (and val log if exists)
-#     if acc_to_error:
-#         for i in range(len(log)):
-#             log[i] = [1.0-x for x in log[i]]
-
-#     plt.figure(figsize=(10, 7.5))
-#     for i in range(len(log)):
-#         plt.plot(log)  
-#     plt.title('acc')
-#     plt.xlabel('epoch')
-#     plt.ylabel('acc')
-#     plt.yticks(yticks)
-#     plt.
-#     plt.show()
+def plot_log(log, , title=None, xlabel='Epoch', ylabel=None, xlim=None, ylim=None xticks=None, yticks=None, # log: dict like {'legend': log}
+            savefig=None, figsize=(4,3), dpi=160):
+    plt.figure(figsize=(4,3), dpi=160)
+    for seq in log.values():
+        plt.plot(seq)
+    plt.legend(log.keys(), loc='upper left')
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.xticks(xticks)
+    plt.yticks(yticks)
+    plt.grid()
+    plt.savefig(savefig)
+    plt.show()
